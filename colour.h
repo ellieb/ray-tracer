9662e7911b7d13823eb48a0f99c94bbd98d9584a
Elise Badun <ebadun@rogers.com>
2020-05-11 09:50:05 -0400

    :sparkles: Add camera header file and anti-aliasing

--- colour.h
+++ colour.h
@@ -5,12 +5,23 @@
 
 #include "vec3.h"
 
-void write_colour(std::ostream &out, colour pixel_colour) {
+void write_colour(std::ostream &out, colour pixel_colour,
+                  int samples_per_pixel) {
+    auto r = pixel_colour.x();
+    auto g = pixel_colour.y();
+    auto b = pixel_colour.z();
+
+    // Divide the color total by the number of samples.
+    auto scale = 1.0 / samples_per_pixel;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
     // Write the translated [0, 255] value of each colour component
-    const float max_val = 255.999;
-    out << static_cast<int>(max_val * pixel_colour.x()) << ' '
-        << static_cast<int>(max_val * pixel_colour.y()) << ' '
-        << static_cast<int>(max_val * pixel_colour.z()) << '\n';
+    const float max_val = 256;
+    out << static_cast<int>(max_val * clamp(r, 0.0, 0.999)) << ' '
+        << static_cast<int>(max_val * clamp(g, 0.0, 0.999)) << ' '
+        << static_cast<int>(max_val * clamp(b, 0.0, 0.999)) << '\n';
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,33 +1,28 @@
-#include "tracer.h"
-#include "hittable_list.h"
-#include "sphere.h"
-#include "colour.h"
-
 #include <iostream>
 
-double hit_sphere(const point3 &center, double radius, const ray &r)
-{
+#include "camera.h"
+#include "colour.h"
+#include "hittable_list.h"
+#include "sphere.h"
+#include "tracer.h"
+
+double hit_sphere(const point3 &center, double radius, const ray &r) {
     vec3 oc = r.origin() - center;
     auto a = r.direction().length_squared();
     auto half_b = dot(oc, r.direction());
     auto c = oc.length_squared() - radius * radius;
     auto discriminant = half_b * half_b - a * c;
-    if (discriminant < 0)
-    {
+    if (discriminant < 0) {
         return -1.0;
-    }
-    else
-    {
+    } else {
         return (-half_b - sqrt(discriminant)) / (a);
     };
 }
 
-colour ray_colour(const ray &r, const hittable &world)
-{
+colour ray_colour(const ray &r, const hittable &world) {
     // Creates a gradient
     hit_record rec;
-    if (world.hit(r, 0, infinity, rec))
-    {
+    if (world.hit(r, 0, infinity, rec)) {
         return 0.5 * (rec.normal + colour(1, 1, 1));
     }
     vec3 unit_direction = unit_vector(r.direction());
@@ -35,35 +30,37 @@ colour ray_colour(const ray &r, const hittable &world)
     return (1.0 - t) * colour(1.0, 1.0, 1.0) + t * colour(0.5, 0.7, 1.0);
 }
 
-int main()
-{
+int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int img_width = 384;
     const int img_height = static_cast<int>(img_width / aspect_ratio);
+    const int samples_per_pixel = 100;
     const float max_val = 255.999;
 
-    std::cout << "P3\n"
-              << img_width << ' ' << img_height << "\n255\n";
+    std::cout << "P3\n" << img_width << ' ' << img_height << "\n255\n";
 
     point3 origin(0.0, 0.0, 0.0);
     vec3 horizontal(4.0, 0.0, 0.0);
     vec3 vertical(0.0, 2.25, 0.0);
-    point3 lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, 1);
+    point3 lower_left_corner =
+        origin - horizontal / 2 - vertical / 2 - vec3(0, 0, 1);
 
     hittable_list world;
     world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+    camera cam;
 
-    for (int j = img_height - 1; j >= 0; --j)
-    {
+    for (int j = img_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-        for (int i = 0; i < img_width; ++i)
-        {
-            auto u = double(i) / (img_width - 1);
-            auto v = double(j) / (img_height - 1);
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-            colour pixel_colour = ray_colour(r, world);
-            write_colour(std::cout, pixel_colour);
+        for (int i = 0; i < img_width; ++i) {
+            colour pixel_colour(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; s++) {
+                auto u = (i + random_double()) / (img_width - 1);
+                auto v = (j + random_double()) / (img_height - 1);
+                ray r = cam.get_ray(u, v);
+                pixel_colour += ray_colour(r, world);
+            }
+            write_colour(std::cout, pixel_colour, samples_per_pixel);
         }
     }
     std::cerr << "\nDone.\n";
